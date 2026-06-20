0683ef0a32c70e7425cca586bda5d89b16107dda
bgashyr648-ops <bgashyr648@gmail.com>
2026-06-20 08:42:15 +0500

    Update dp.js

--- plugins/dp.js
+++ plugins/dp.js
@@ -7,22 +7,16 @@ cmd({
     react: "🖼️",
     filename: __filename
 }, async (conn, mek, m, { reply, args }) => {
+    // Check agar number dia gaya hai
     let input = args[0];
     if (!input) return reply("*Bhai, koi number toh likho! Maslan: .getpp 923001234567*");
 
+    // Number ko format karna
     let number = input.replace(/[^0-9]/g, '') + "@s.whatsapp.net";
     
     try {
         let ppUrl = await conn.profilePictureUrl(number, 'image');
-        
-        // Yahan 'm.chat' ko fix kiya hai taake ye personal aur group dono mein chale
-        let jid = m.chat || m.sender;
-        
-        await conn.sendMessage(jid, { 
-            image: { url: ppUrl }, 
-            caption: "*Love MD - Profile Pic Downloaded*" 
-        }, { quoted: mek });
-        
+        await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption: "*Love MD - Profile Pic Downloaded*" }, { quoted: mek });
     } catch (e) {
         reply("*Bhai, ya toh number galat hai ya phir uski DP private hai.*");
     }
